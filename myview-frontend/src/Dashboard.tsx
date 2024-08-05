56476bcd337b5f79a42250f6c214ce6c567a5a33
Eno Ren <enoren@163.com>
2024-08-05 16:14:15 -0700

    This is the update for homepage UI.

--- myview-frontend/src/Dashboard.tsx
+++ myview-frontend/src/Dashboard.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import { UserOutlined, VideoCameraOutlined, HomeOutlined, SettingOutlined, ShopOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme,Row, Col, Typography, Button,Card, Avatar } from 'antd';
 import { useParams } from 'react-router-dom';
 
 const { Header, Content, Sider } = Layout;
+const { Title } = Typography;
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-  (icon, index) => ({
-    key: String(index + 1),
-    icon: React.createElement(icon),
-    label: `nav ${index + 1}`,
-  }),
-);
+const items = [
+  { key: '1', icon: React.createElement(HomeOutlined), label: 'Home' },
+  { key: '2', icon: React.createElement(UserOutlined), label: 'Character Panel' },
+  { key: '3', icon: React.createElement(VideoCameraOutlined), label: 'Story Creation' },
+  { key: '4', icon: React.createElement(ShopOutlined), label: 'Community Overview' },
+  { key: '5', icon: React.createElement(SettingOutlined), label: 'Settings' },
+];
 
 const Dashboard: React.FC = () => {
   const { username } = useParams<{ username: string }>();
@@ -20,7 +21,7 @@ const Dashboard: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout style={{ minHeight: '100vh' }}> {/* Layout can fill the whole window */}
+    <Layout style={{ minHeight: '100vh' }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
@@ -30,22 +31,79 @@ const Dashboard: React.FC = () => {
         onCollapse={(collapsed, type) => {
           console.log(collapsed, type);
         }}
+        style={{ background: "white" }}
       >
-        <div className="naviagtor" />
-        <Menu theme="light" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <div className="navigator">
+          <div style={{ padding: '16px', fontSize: '24px'}}>{username}</div>
+          <div style={{ padding: '16px', fontSize: '16px'}}>navigator</div>
+          <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} items={items} />
+        </div>
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+      <Header style={{ padding: 0, background: '#fff' }}>
+        <Title level={1} style={{ textAlign: 'center' }}>
+              Welcome Back To {username}
+        </Title>
+      </Header>
         <Content style={{ margin: '24px 16px 0' }}>
           <div
             style={{
               padding: 24,
               minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
+              background: '#fff',
+              borderRadius: '8px',
             }}
           >
-            Welcome, {username}!
+            
+            <Title level={3} style={{ textAlign: 'center' }}>
+              Your Team Recent Activities
+            </Title>
+            <Row gutter={16} style={{ marginTop: '24px' }}>
+              <Col span={8}>
+                <Card title="Add X Figure" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=Zoe" alt='avatar'/>}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Modify Settings" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=Max" alt="avatar" />}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Add a new scenario" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=Bob" alt="avatar" />}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Update X character" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=George" alt="avatar" />}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Release a New Story" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=Kitty" alt="avatar" />}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Create a new story" extra={<Avatar src="https://api.dicebear.com/9.x/pixel-art/svg?seed=Jasper" alt="avatar" />}>
+                  <p>Name</p>
+                  <p>Time</p>
+                </Card>
+              </Col>
+            </Row>
+            <div style={{ textAlign: 'center', marginTop: '24px' }}>
+              <Button type="primary" style={{ marginRight: '16px' }}>
+                Create A New Story
+              </Button>
+              <Button type="primary">
+                Create A Character
+              </Button>
+            </div>
           </div>
         </Content>
       </Layout>
